445d1b87645730d96a9e5c06c6f0e8afaa78b7b9
tutorialcreation <tutorialcreation81@gmail.com>
2022-06-15 05:01:18 +0300

    complete setting up of backend

---
+++ server/apps/notes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include 
+from rest_framework.routers import DefaultRouter
+from apps.notes.views import NoteViewSet
+
+router = DefaultRouter()
+router.register("notes", NoteViewSet, basename="notes")
+notes_urlpatterns = [path("api/v1/", include(router.urls))]
